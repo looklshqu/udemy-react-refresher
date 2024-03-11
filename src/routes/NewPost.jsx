b789ee86dde323def9eec45cabdd806fef8d1149
looklsh <looklsh38@gmail.com>
2024-03-11 14:31:36 +0900

    2강완료 20240311

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,54 +1,62 @@
-import { Link } from "react-router-dom";
+import { Link, Form, redirect } from "react-router-dom";
 import Modal from "../components/Modal";
 import classes from "./NewPost.module.css";
 import { useState } from "react";
 
-function NewPost({ onAddPost }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
+function NewPost() {
+  // const [enteredBody, setEnteredBody] = useState("");
+  // const [enteredAuthor, setEnteredAuthor] = useState("");
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
+  // function bodyChangeHandler(event) {
+  //   setEnteredBody(event.target.value);
+  // }
 
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
+  // function authorChangeHandler(event) {
+  //   setEnteredAuthor(event.target.value);
+  // }
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(`name: ${e.target.name}, value: ${e.target.value}`);
-    const postData = {
-      body: enteredBody,
-      author: enteredAuthor,
-    };
-    onAddPost(postData);
-    onCancel();
-  };
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   console.log(`name: ${e.target.name}, value: ${e.target.value}`);
+  //   const postData = {
+  //     body: enteredBody,
+  //     author: enteredAuthor,
+  //   };
+  //   onAddPost(postData);
+  //   onCancel();
+  // };
 
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <Form method="post" className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+          <textarea id="body" name="body" required rows={3} />
         </p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input
-            type="text"
-            id="name"
-            required
-            onChange={authorChangeHandler}
-          />
+          <input type="text" id="name" name="author" required />
         </p>
         <p className={classes.actions}>
           <Link to="..">Cancel</Link>
           <button>Submit</button>
         </p>
-      </form>
+      </Form>
     </Modal>
   );
 }
 
 export default NewPost;
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData); // {body: '...', author: '...'}
+  await fetch("http://localhost:8080/posts", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return redirect("/");
+}
